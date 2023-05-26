73f0da7e3f09323ac79e3b27e4b8a09d8ded5586
Light <Light010191@yandex.ru>
2023-05-26 21:08:35 +0300

    first commit

---
+++ Vacancy_Store/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Vacancy_Store.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
