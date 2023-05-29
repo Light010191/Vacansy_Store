7852260a310917e998266124df2f66e5bdfd8dab
Light <Light010191@yandex.ru>
2023-05-29 20:02:58 +0300

    upd

--- Vacancy_Store/obj/Debug/net6.0-windows/Views/Windows/MainWindow.g.i.cs
+++ Vacancy_Store/obj/Debug/net6.0-windows/Views/Windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace Vacancy_Store.Views.Windows {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Vacancy_Store;V1.0.0.0;component/views/windows/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Vacancy_Store;component/views/windows/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Windows\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
