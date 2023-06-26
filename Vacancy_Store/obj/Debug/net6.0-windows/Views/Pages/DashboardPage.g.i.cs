c69aca59a1d307add8375084c4264dd4f59234f5
Hankerman <stalker-2204@mail.ru>
2023-06-26 18:16:29 +0300

    r

--- Vacancy_Store/obj/Debug/net6.0-windows/Views/Pages/DashboardPage.g.i.cs
+++ Vacancy_Store/obj/Debug/net6.0-windows/Views/Pages/DashboardPage.g.i.cs
@@ -93,7 +93,7 @@ namespace Vacancy_Store.Views.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Vacancy_Store;component/views/pages/dashboardpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Vacancy_Store;V1.0.0.0;component/views/pages/dashboardpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Pages\DashboardPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
