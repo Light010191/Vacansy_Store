4af2b7a877d1bb984c58647630a22b60ecc61836
FedayevR <fedayevDev@yandex.ru>
2023-06-29 23:09:09 +0700

    fix

--- Vacancy_Store/obj/Debug/net6.0-windows/Views/Windows/GRUDResumeWindow.g.i.cs
+++ Vacancy_Store/obj/Debug/net6.0-windows/Views/Windows/GRUDResumeWindow.g.i.cs
@@ -30,9 +30,6 @@ using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Shell;
-using Vacancy_Store.Helpers;
-using Vacancy_Store.Models;
-using Vacancy_Store.Views.Pages;
 using Vacancy_Store.Views.Windows;
 
 
@@ -50,7 +47,7 @@ namespace Vacancy_Store.Views.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -66,7 +63,7 @@ namespace Vacancy_Store.Views.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
